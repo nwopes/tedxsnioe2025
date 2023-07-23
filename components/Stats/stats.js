6fea1f966b7e502d3ee642dc7136fef070c0f4ff
Ary0405 <aryansethia2003@gmail.com>
2023-07-23 13:02:20 +0530

    stats section done

--- components/Stats/stats.js
+++ components/Stats/stats.js
@@ -1,53 +1,23 @@
-import styles from "./stats.module.css";
-const Stats = () => {
+import './stats.scss'
+export default function Stats() {
   return (
-    <div className={styles.stats}>
-      <img
-        className={styles.websitepattern1Icon}
-        alt=""
-        src="/websitepattern-1@2x.png"
-      />
-      <div className={styles.attendees}>
-        <p className={styles.p}>
-          <span>
-            <span>650</span>
-            <span className={styles.span}>+</span>
-          </span>
-        </p>
-        <p className={styles.attendees1}>Attendees</p>
+    <div className='StatsContainer'>
+      <div className='StatsContainer__card'>
+        <p className='StatsContainer__card--number'>650+</p>
+        <p className='StatsContainer__card--text'>Attendees</p>
       </div>
-      <div className={styles.attendees2}>
-        <p className={styles.p}>
-          <span>
-            <span>650</span>
-            <span className={styles.span}>+</span>
-          </span>
-        </p>
-        <p className={styles.attendees3}>Attendees</p>
+      <div className='StatsContainer__card'>
+        <p className='StatsContainer__card--number'>650+</p>
+        <p className='StatsContainer__card--text'>Attendees</p>
       </div>
-      <div className={styles.attendees4}>
-        <p className={styles.p}>
-          <span>
-            <span>650</span>
-            <span className={styles.span}>+</span>
-          </span>
-        </p>
-        <p className={styles.attendees5}>Attendees</p>
+      <div className='StatsContainer__card'>
+        <p className='StatsContainer__card--number'>650+</p>
+        <p className='StatsContainer__card--text'>Attendees</p>
       </div>
-      <div className={styles.attendees6}>
-        <p className={styles.p}>
-          <span>
-            <span>650</span>
-            <span className={styles.span}>+</span>
-          </span>
-        </p>
-        <p className={styles.attendees7}>Attendees</p>
+      <div className='StatsContainer__card'>
+        <p className='StatsContainer__card--number'>650+</p>
+        <p className='StatsContainer__card--text'>Attendees</p>
       </div>
-      <div className={styles.statsChild} />
-      <div className={styles.statsItem} />
-      <div className={styles.statsInner} />
     </div>
-  );
-};
-
-export default Stats;
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import LandingSection from "../components/LandingSection/LandingSection";
+import Stats from "../components/Stats/stats";
 import '../styles/routes/home.scss'
 function Home() {
     return (
-        <section className="LandingPageSection">
-            <LandingSection />
-        </section>
+        <>
+            <section className="LandingPageSection">
+                <LandingSection />
+            </section>
+            <section className="StatsSection">
+                <Stats />
+            </section>
+        </>
     )
 }
 
