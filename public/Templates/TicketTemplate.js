903a3c7bdac6f73f1302e8829b51c9f80838a8e3
Ary0405 <aryansethia2003@gmail.com>
2023-09-20 01:14:50 +0530

    ticket changes

--- public/Templates/TicketTemplate.js
+++ public/Templates/TicketTemplate.js
@@ -11,14 +11,12 @@ export const TicketTemplate = () => {
 
 <body>
     <div>
-        <p>Greetings, Early Bird!<br/><br />
+        <p>Greetings, Future-Conference-Goer!<br/><br />
 
-            You're a part of the first bunch of people who have snagged tickets to the biggest event on campus at a
-            lowered price, and we can’t wait to see you at the conference. <br/><br/>
+            You're a part of the first round of people who have snagged tickets to the biggest event on campus, and we can’t wait to see you at the conference.<br/><br/>
 
-            We've noticed some of you could not get a confirmation through the website, as it couldn’t handle the
-            traffic. Our sincere apologies for the inconvenience, please consider this email to be your confirmation!<br/><br/>
-
+            We've noticed some of you could not get a confirmation through the website, as it couldn’t handle the traffic. Our sincere apologies for the inconvenience, please consider this email to be your confirmation!<br/><br/>
+            
             You are officially on board. All systems operational.<br/><br/>
 
             With love,<br/>
