fcfe3a36f929c6b818c3e6508f186a9383a245af
turtleturtleturtleturtleturtle <at173@snu.edu.in>
2025-09-23 21:25:44 +0530

    Update contact.js
    Changed names to the current team

--- pages/contact.js
+++ pages/contact.js
@@ -8,18 +8,18 @@ function Contact()
                 <p className='ContactUsSection__container--heading'>Contact Us</p>
                 <div className='ContactUsSection__container--details'>
                     <div className='ContactUsSection__container--details__card'>
-                        <p className='ContactUsSection__container--details__card--name'>Snehali Biswas</p>
+                        <p className='ContactUsSection__container--details__card--name'>Dipro Mukherjee</p>
                         <p className='ContactUsSection__container--details__card--designation'>Lead Organizer</p>
-                        <p className='ContactUsSection__container--details__card--phone'>+91-9740746511</p>
+                        <p className='ContactUsSection__container--details__card--phone'>+91-7032536333</p>
                     </div>
                     <div className='ContactUsSection__container--details__card'>
-                        <p className='ContactUsSection__container--details__card--name'>Gautam Nanda</p>
+                        <p className='ContactUsSection__container--details__card--name'>Aanya Anand</p>
                         <p className='ContactUsSection__container--details__card--designation'>Co Organizer</p>
-                        <p className='ContactUsSection__container--details__card--phone'>+91-7377943130</p>
+                        <p className='ContactUsSection__container--details__card--phone'>+91-9622816781</p>
                     </div><div className='ContactUsSection__container--details__card'>
-                        <p className='ContactUsSection__container--details__card--name'>Dwiti Modi</p>
+                        <p className='ContactUsSection__container--details__card--name'>Anshika Gupta</p>
                         <p className='ContactUsSection__container--details__card--designation'>Finance Head</p>
-                        <p className='ContactUsSection__container--details__card--phone'>+91-9316244424</p>
+                        <p className='ContactUsSection__container--details__card--phone'>+91-7358012257</p>
                     </div>
                 </div>
             </div>
