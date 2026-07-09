8451feaf72e83784419b7037f12898d6539e3ad4
Ekansh Jain <95521249+Anonymous-Ekansh@users.noreply.github.com>
2026-07-09 15:34:49 +0530

    Remove Sponsors section and add TEDx explanation
    Removed Sponsors section and added a new section explaining TEDx with a link to learn more.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import AboutUs from "../components/AboutUs/AboutUs";
 import LandingSection from "../components/LandingSection/LandingSection";
 import PastSpeakers from "../components/PastSpeakers/PastSpeakers";
-import Sponsors from "../components/Sponsors/Sponsors";
 
 import '../styles/routes/home.scss'
 function Home() {
@@ -25,9 +24,18 @@ function Home() {
             </section>
             
             <section
-                id="sponsors"
-                className="SponsorsSection">
-                <Sponsors />
+                id="what-is-tedx"
+                className="SponsorsSection"
+                style={{ padding: "5rem 2rem", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                <h2 style={{ fontSize: "4.25rem", marginBottom: "2rem", color: "white", fontWeight: "400", fontFamily: '"Bebas Neue", serif' }}>
+                    What is &nbsp;<span style={{ color: "#eb0028", fontFamily: 'Helvetica, Arial, sans-serif', fontWeight: "bold", letterSpacing: "-2px" }}>TED<sup style={{ fontSize: "0.6em", letterSpacing: "0", marginLeft: "2px", verticalAlign: "super" }}>x</sup></span>
+                </h2>
+                <p style={{ maxWidth: "800px", margin: "0 auto", fontSize: "1.3rem", lineHeight: "1.6", color: "white", fontFamily: '"Outfit", sans-serif', textAlign: "justify" }}>
+                    In the spirit of ideas worth spreading, TED has created a program called TEDx. TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. Our event is called TEDxShivNadarUniversity, where x = independently organized TED event. At our TEDxShivNadarUniversity event, TEDTalks video and live speakers will combine to spark deep discussion and connection in a small group. The TED Conference provides general guidance for the TEDx program, but individual TEDx events, including ours, are self-organized.
+                </p>
+                <a href="https://www.ted.com/about/programs-initiatives/tedx-program" target="_blank" rel="noopener noreferrer" style={{ marginTop: "1.5rem", color: "#eb0028", fontFamily: '"Outfit", sans-serif', fontSize: "1.2rem", textDecoration: "underline", fontWeight: "bold" }}>
+                    Learn more about the TEDx program
+                </a>
             </section>
         </>
     )
